77f0a20cc09921bb65ef0758b7aba356ab90fb74
bjornBEs <bjorn@r66.dk>
2026-09-10 12:23:30 +0200

    Add project files.

---
+++ Chess.cs
@@ -0,0 +1,120 @@
+﻿using System.Text;
+
+namespace BasisProgrammeringOpgave
+{
+    [Flags]
+    enum PieceInfo : uint
+    {
+        TeamWhite = 0b000_000_0,
+        TeamBlack = 0b000_000_1,
+        PieceTypeEmpty = 0b000_000_0,
+        PieceTypePawn = 0b000_001_0,
+        PieceTypeKnight = 0b000_010_0,
+        PieceTypeBishop = 0b000_011_0,
+        PieceTypeRook = 0b000_100_0,
+        PieceTypeQueen = 0b000_101_0,
+        PieceTypeKing = 0b000_110_0,
+        PieceTypeMask = 0b000_111_0,
+    }
+    public static class Chess
+    {
+        // ♖ ♘ ♗ ♕ ♔ ♙
+        static char[,] borad;
+        // bit field for pice info
+        static PieceInfo[,] boradInfo;
+        public static void StartChessGame()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            borad = new char[8, 8]
+            {
+                { '♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖' },
+                { '♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                { '♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙' },
+                { '♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖' },
+            };
+            boradInfo = new PieceInfo[8, 8]
+            {
+                { PieceInfo.PieceTypeRook, PieceInfo.PieceTypeKnight, PieceInfo.PieceTypeBishop, PieceInfo.PieceTypeQueen, PieceInfo.PieceTypeKing, PieceInfo.PieceTypeBishop, PieceInfo.PieceTypeKnight, PieceInfo.PieceTypeRook },
+                { PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn },
+                { PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty },
+                { PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty },
+                { PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty },
+                { PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty, PieceInfo.PieceTypeEmpty },
+                { PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn, PieceInfo.PieceTypePawn },
+                { PieceInfo.PieceTypeRook, PieceInfo.PieceTypeKnight, PieceInfo.PieceTypeBishop, PieceInfo.PieceTypeQueen, PieceInfo.PieceTypeKing, PieceInfo.PieceTypeBishop, PieceInfo.PieceTypeKnight, PieceInfo.PieceTypeRook },
+            };
+
+            for (int y = 0; y < boradInfo.GetLength(0); y++)
+            {
+                for (int x = 0; x < boradInfo.GetLength(1); x++)
+                {
+                    if (y == 0 || y == 1)
+                    {
+                        boradInfo[y, x] |= PieceInfo.TeamBlack;
+                    }
+                    if (y == 6 || y == 7)
+                    {
+                        boradInfo[y, x] |= PieceInfo.TeamWhite;
+                    }
+                }
+            }
+
+            Console.WriteLine("Welcome to 2d chess - by Bjorn");
+            Console.WriteLine("This game can be played aginst vs an AI or vs another player");
+            Console.WriteLine("Write 'AI' or 1 to fight aginst an AI");
+            Console.WriteLine("Write 'Player' or 2 to fight aginst another player");
+            string input = Console.ReadLine();
+
+            bool aginst = false;
+            if (!int.TryParse(input, out int value))
+            {
+            }
+
+            while (true)
+            {
+                PrintBoard();
+
+                GetInput();
+            }
+        }
+
+        static void GetInput()
+        {
+
+        }
+
+        static void PrintBoard()
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int y = 0; y < boradInfo.GetLength(0); y++)
+            {
+                Console.Write($"{y + 1} "); // Row label
+
+                for (int x = 0; x < boradInfo.GetLength(1); x++)
+                {
+                    if ((x + y) % 2 == 1) // black background for uneven, white for even.
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+                    PieceInfo piece = boradInfo[y, x];
+                    if ((piece & PieceInfo.PieceTypeMask) == PieceInfo.PieceTypeEmpty)
+                    {
+                        Console.Write("  ");
+                    }
+                    else
+                    {
+                        Console.Write(borad[y, x] + " ");
+                    }
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h"); // column labels
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿namespace BasisProgrammeringOpgave;
+
+class Program
+{
+    /// <summary>
+    /// Makes a new menu with options and input
+    /// </summary>
+    /// <param name="options">An array of options to choose</param>
+    /// <param name="choice">The returning choice from options or String.Empty if the function returns -1</param>
+    /// <returns>The index in options that the choice</returns>
+    static int OpenMenu(string[] options, out string choice)
+    {
+        int cursor = 0;
+
+        while (true)
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < options.Length; i++)
+            {
+                if (i == cursor)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+
+                Console.WriteLine($"\t{options[i]}");
+                Console.ResetColor();
+            }
+
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.Key == ConsoleKey.Escape)
+            {
+                choice = String.Empty;
+                return -1;
+            }
+            else if (key.Key == ConsoleKey.Enter)
+            {
+                choice = options[cursor];
+                return cursor;
+            }
+            else if (key.Key == ConsoleKey.UpArrow && cursor != 0)
+            {
+                cursor--;
+            }
+            else if (key.Key == ConsoleKey.DownArrow && cursor < options.Length - 1)
+            {
+                cursor++;
+            }
+        }
+    }
+
+    static void choiceWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+        Console.WriteLine("In this program can you play these games");
+        Console.WriteLine("option1, option2 and option3");
+        Console.WriteLine("you also also write 'help' to print the help text");
+    }
+
+    static void choiceOption1()
+    {
+    }
+
+    static void choiceOption2()
+    {
+    }
+
+    static void choiceOption3()
+    {
+        Chess.StartChessGame();
+    }
+
+    static void choiceHelp()
+    {
+        Console.WriteLine("option1  - Will play the ");
+        Console.WriteLine("option2  - Will play the ");
+        Console.WriteLine("chess    - Will play chess");
+        Console.WriteLine("help     - Will show this text");
+        Console.WriteLine("credit   - Will show who was apart of this program");
+        Console.WriteLine("exit     - Will close the program");
+    }
+
+    static void choiceCredit()
+    {
+        Console.Clear();
+        string[] credits =
+        {
+            "BjornBEs - Made the menu",
+            "BjornBEs - Made chess",
+            "@ BjornBEs - Made the menu2"
+        };
+
+        int middle = Console.BufferWidth / 2;
+        for (int i = 0; i < credits.Length; i++)
+        {
+            string s = credits[i];
+            int length = s.Length;
+            int point = middle - (length / 2);
+            Console.SetCursorPosition(point, i);
+            Console.WriteLine(s);
+        }
+
+        Console.WriteLine("press any key to continue.");
+        Console.ReadLine();
+        Console.Clear();
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.ResetColor();
+        Console.CursorVisible = false;
+
+        string[] options = { "option1", "option2", "option3", "help", "credit", "exit" };
+        Action[] actions = { choiceOption1, choiceOption2, choiceOption3, choiceHelp, choiceCredit };
+        while (true)
+        {
+            int index = OpenMenu(options, out string choice);
+            if (index < actions.Length)
+            {
+                Console.WriteLine();
+                actions[index]();
+            }
+
+            switch (choice)
+            {
+                case "exit":
+                    // could also do return in Main but Environment.Exit reads more clean
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+}
