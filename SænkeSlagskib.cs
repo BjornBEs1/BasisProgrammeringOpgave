d718f5fdbce6232ee79486c5003b60bd4c79b051
RasmusGammelby <raga7@student.eadania.dk>
2026-09-11 20:07:47 +0200

    En lille og meget simpel start menu. Der er oprettet både computer- og spillerbræt, så det nu er synligt for spilleren.

--- SænkeSlagskib.cs
+++ SænkeSlagskib.cs
@@ -1,14 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BasisProgrammeringOpgave
+﻿namespace BasisProgrammeringOpgave
 {
     class SænkeSlagskib
     {
         public static void Start()
         {
+            Console.WriteLine("Welcome to Sænke Slagskib!");
+            Console.WriteLine("Press any key to start the game");
+            Console.WriteLine();
+            Console.ReadKey();
 
+            Console.WriteLine("OPTIONS MENU");
+            Console.WriteLine("Ready to play? (yes/no)");
+            Console.WriteLine("Settings");
+
+
+
+            string ready = Console.ReadLine(); //Spillerens input (yes) læses ind og gemmes i variablen "ready"
+            if (ready == "yes")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Deploying ships...");
+                Console.WriteLine("Ships deployed! Let the battle begin!");
+                Console.WriteLine();
+                    
+                char[,] computerboard = new char[6, 6]; // Computerens bræt oprettes som et 6x6 gitter
+                for (int i = 0; i < 6; i++) // Ydre løkke , der går gennem hver række i computerens bræt
+                {
+                    for (int j = 0; j < 6; j++) // Indre løkke, der går gennem hver kolonne i computerens bræt
+                    { 
+                      computerboard[i, j] = '0'; // Hvert fældt i computerens bræt sættes til 0 
+                    }                       
+                }
+                PrintBoard(computerboard);
+                void PrintBoard(char[,] a)
+                {
+                    for (int i = 0; i < 6; i++)
+                    {
+                         for (int j = 0; j < 6; j++)
+                         {
+                             Console.Write(a[i, j] + " "); // Hvert fældt i computerens bræt udskrives til konsollen, så det kan ses af spilleren
+                         }
+                             Console.WriteLine();
+                    }
+
+                }
+                Console.WriteLine();
+
+                char[,] playerboard = new char[6, 6]; // Spillerens bræt oprettes som et 6x6 gitter
+                for (int i = 0; i < 6; i++) // Ydre løkke , der går gennem hver række i spillerens bræt
+                {
+                    for (int j = 0; j < 6; j++) // Indre løkke, der går gennem hver kolonne i spillerens bræt
+                    {
+                        playerboard[i, j] = '0'; // Hvert fældt i spillerens bræt sættes til 0 
+                    }
+                }
+                PrintBoard2(playerboard);
+                void PrintBoard2(char[,] a)
+                {
+                    for (int i = 0; i < 6; i++)
+                    {
+                        for (int j = 0; j < 6; j++)
+                        {
+                            Console.Write(a[i, j] + " "); // Hvert fældt i spillerens bræt udskrives til konsollen, så det kan ses af spilleren
+                        }
+                            Console.WriteLine();
+                    }
+                }
+
+
+
+            }
+            else if (ready == "no")
+            {
+
+
+                Console.WriteLine("Okay, maybe next time ):");
+            }
         }
     }
 }
