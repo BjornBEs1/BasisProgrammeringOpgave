89279d9655948b23f135f75460dc837c8df7f4c1
RasmusGammelby <raga7@student.eadania.dk>
2026-09-20 21:23:04 +0200

    Fino

--- SænkeSlagskib.cs
+++ SænkeSlagskib.cs
@@ -7,7 +7,7 @@ namespace BasisProgrammeringOpgave
 
         public static void Start()
         {
-            Console.WriteLine("Welcome to Sænke Slagskib!");
+            Console.WriteLine("Welcome to Sænke Slagskib! (Made by Rasmus)");
             Console.WriteLine("Press any key to start the game");
             Console.WriteLine();
             Console.ReadKey();
@@ -114,25 +114,25 @@ namespace BasisProgrammeringOpgave
                         playerBoard[i, j] = '0'; // Hvert felt i spillerens bræt sættes til 0. 
                     }
                 }
-                foreach (int size in shipSize)
+                foreach (int size in shipSize) // Foreach, så der vælges et skib ad gangen - Sørger for at gå videre til næste skib, når et skib placeres korrekt.
                 {
                     bool placed = false;
 
-                    while (!placed)
+                    while (!placed) // While-løkke der kører indtil hvert skib enkeltvis er placeret korrekt
                     {
                         Console.WriteLine($"Placer dit skib på længde {size}");
                         Console.WriteLine("Skriv række og kolonne Fx: 0 0 eller 9 9");
-                        string[] input = Console.ReadLine().Split(' ');
-                        int row = int.Parse(input[0]);
+                        string[] input = Console.ReadLine().Split(' '); // deler spilleren input med et mellemrum. Vi får et array Fx [3, 7]
+                        int row = int.Parse(input[0]); // Et spilbræt i C# er et 2D array, så vi kan kun bruge heltal. Derfor "tekst -> heltal(int).  
                         int col = int.Parse(input[1]);
 
                         Console.WriteLine("Vandret eller lodret? (Tast: v/l)");
-                        string direction = Console.ReadLine();
+                        string direction = Console.ReadLine(); // Spilleren vælger I hvilken retning skibet skal vende (Vandret eller lodret).
                         bool vandret = direction == "v";
 
-                        bool free = true;
+                        bool free = true; // Variablen "free" bruges til at afgøre om skibet kan placeres uden overlap eller at gå ud over kanten.
 
-                        if (vandret)
+                        if (vandret) // En if-sætning, der bruges til at finde ud af om ovenstående er muligt.
                         {
                             if (col + size > 10)
                                 free = false;
@@ -172,18 +172,18 @@ namespace BasisProgrammeringOpgave
                             Console.WriteLine();
                             Console.WriteLine("Ship deployed!");
                             Console.WriteLine();
-                            placed = true;
+                            placed = true; // Skib placeret.
 
                         }
                         else
                         {
-                            Console.WriteLine();
+                            Console.WriteLine(); // Skib ikke placeret.
                             Console.WriteLine("Can't deploy the ship here, Sir! The position might already be taken or the ship might be too big for the position, Sir! (Place your ship between 0 0 & 9 9");
                             Console.WriteLine();
                         }
 
                     }
-                    Console.Clear();
+                    Console.Clear(); // Bruges til at fjerne en allerede gennemført del af spillet -> Giver et mere clean look.
 
 
                 }
@@ -194,7 +194,7 @@ namespace BasisProgrammeringOpgave
                     Console.Clear();
                 Console.WriteLine("THE ENEMY");
                 Console.WriteLine();
-                PrintBoard(computerBoard);
+                PrintBoard(computerBoard); // Computerens bræt med skibe synliggøres.
 
                 Console.WriteLine();
 
@@ -207,7 +207,7 @@ namespace BasisProgrammeringOpgave
                     {
                         for (int j = 0; j < 10; j++)
                         {
-                            Console.Write(a[i, j] + " "); // Hvert felt i spillerens bræt udskrives til konsollen, så det kan ses af spilleren.
+                            Console.Write(a[i, j] + " "); // Spillerens bræt med skibe synliggøres.
                         }
                         Console.WriteLine();
                     }
@@ -215,27 +215,63 @@ namespace BasisProgrammeringOpgave
 
                 while (true)
                 {
-                    PlayerShoot(computerBoard); // Spilleren skyder
+                    PlayerShoot(computerBoard); // Spilleren skyder.
                     {
-                        if (AllShipsSunk(computerBoard))
+                        if (AllShipsSunk(computerBoard)) // En funktion, der afslutter spillet, når alle skibe er sunket på enten computerens eller spillerens bræt.
                         {
                             Console.WriteLine("WE WON THE BATTLE, CAPTAIN!");
-                            Thread.Sleep(3000);
+                            Thread.Sleep(3000); // Får Console.WriteLine til at forblive synlig i fx 3 sekunder, før teksten så forsvinder igen.
+
+                            GameOverAnimation(); // En sjov GAME OVER animation. 
+
+                            string choice = EndOfGameMenu(); // Spilleren får mulighed for at starte et nyt spil eller gå tilbage til hovedmenuen.
+                            if (choice == "1")
+                            {
+                                Console.ResetColor();
+                                Console.Clear();
+                                Start(); // Starter nyt spil. 
+                            }
+                            else if (choice == "2")
+                            {
+                                
+                                Console.ResetColor();
+                                Console.Clear();
+                                return; // Tilbage til hovedmenuen.
+                            }
+
                             break; // Spillet stopper, fordi spilleren vandt
                         }
                     }
                     computerShoot(playerBoard); // Computeren skyder
-                    {
+                    
                         if (AllShipsSunk(playerBoard))
                         {
                             Console.WriteLine("We lost, Captain");
                             Thread.Sleep(3000);
+
+                            GameOverAnimation();
+
+                            string choice = EndOfGameMenu();
+
+                            if (choice == "1")
+                            {
+                                Console.Clear();
+                                Start();
+                            }
+                            else if (choice == "2")
+                            {
+                                
+                                Console.ResetColor();
+                                Console.Clear();
+                                return;
+                            }
+                            
                             break; // Spillet stopper, fordi computeren vandt
                         }
-                    }
+                    
 
                     Console.Clear();
-                    Console.WriteLine("THE ENEMY"); // Begge bræt opdateres
+                    Console.WriteLine("THE ENEMY"); // Begge bræt opdateres efter hvert skud.
                     PrintBoard(computerBoard);
 
                     Console.WriteLine();
@@ -257,27 +293,68 @@ namespace BasisProgrammeringOpgave
         }
         public static void PlayerShoot(char[,] computerBoard)
         {
-            Console.WriteLine("Fire the canons!");
+            Console.WriteLine("Fire the canons!"); // Muligheder for spilleren, som vises når spillet er i gang.
             Console.WriteLine("Fx: 0 5 or 4 7");
-            string[] shot = Console.ReadLine().Split(' ');
+            Console.WriteLine("Type: NUKE -> destroy all enemy ships instantly");
+
+            string input = Console.ReadLine().Trim();
+
+            //NUKE
+            if (input.ToUpper() == "NUKE") // Hurtigt WIN!
+            {
+                NukeEnemyFleet(computerBoard); // En "NUKE" funktion, der gør det muligt for spilleren altid at afgøre spillet hurtigt i tilfælde af fx aftensmad.
+
+                if(AllShipsSunk(computerBoard))
+                {
+                    Console.WriteLine("Enemy fleet destroyed! We are victorious!");
+                    Thread.Sleep(2000);
+
+                    GameOverAnimation();
+
+                    string choice = EndOfGameMenu();
+
+                    if (choice == "1")
+                    {
+                        Console.Clear();
+                        Start();
+                    }
+                    else if (choice == "2")
+                    {
+                        
+                        Console.ResetColor();
+                        Console.Clear();
+                        return;
+                    }
+                }
+                return;
+            }
+            // Koordinatskydning
+            string[] shot = input.Split(' ', StringSplitOptions.RemoveEmptyEntries); // Fjerner ekstra mellemrum hvis spilleren ved en fejl placerer dem.
+
+            if (shot.Length != 2)
+            {
+                Console.WriteLine("Invalid input. Type row and column like: 6 5");
+                return;
+            }
+
             int row = int.Parse(shot[0]);
             int col = int.Parse(shot[1]);
 
-            if (computerBoard[row, col] == 'x')
+            if (computerBoard[row, col] == 'x') // En neutral eller ukendt position er markeret med et "X".
             {
-                computerBoard[row, col] = 'H';
-                Console.WriteLine("It´s a HIT!");
+                computerBoard[row, col] = 'H'; // Når et skib rammes markeres positionen med "H"(Hit).
+                Console.WriteLine("It´s a HIT!"); 
                 Thread.Sleep(3000);
             }
             else if (computerBoard[row, col] == '0')
             {
-                computerBoard[row, col] = 'M';
+                computerBoard[row, col] = 'M'; // En forbier makeres med et "M"(Miss"
                 Console.WriteLine("It´s a miss");
                 Thread.Sleep(3000);
             }
             else
             {
-                Console.WriteLine("This position have already been hit, Sir");
+                Console.WriteLine("This position have already been hit, Sir"); // Hvis positionen allerede er blevet beskudt.
                 Thread.Sleep(3000);
             }
         }
@@ -301,22 +378,101 @@ namespace BasisProgrammeringOpgave
             }
             else
             {
-                computerShoot(playerBoard); // Skyder igen hvis feltet er brugt
+                computerShoot(playerBoard);
             }
             
         }
-        public static bool AllShipsSunk(char[,] board) // Tjekker om alle skibe er ramt
+        public static bool AllShipsSunk(char[,] board) // Tjekker om alle skibe er sunket.
         {
             for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
                     if (board[i, j] == 'x')
-                        return false; // Der er stadig et eller flere skibe
+                        return false; // Der er stadig et eller flere skibe tilbage.
                 }
             }
 
             return true; // Ingen skibe tilbage = tabt eller vundet
         }
+        public static void GameOverAnimation()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green; // GAME OVER skrives med grøn tekst.
+
+            string[] frames =
+            {
+                "G",
+                "GA",
+                "GAM",
+                "GAME",
+                "GAME O",
+                "GAME OV",
+                "GAME OVE",
+                "GAME OVER",
+                "GAME OVER!",
+                "GAME OVER!!"
+            };
+
+            foreach (string frame in frames)
+            {
+                Console.Clear();
+                Console.WriteLine(frame);
+                Thread.Sleep(200); // 0,2 sek pr. Frame
+
+            }
+
+            Console.ResetColor();
+            Thread.Sleep(1000);
+        }
+        public static string EndOfGameMenu() // Spilleren for et valg om at starte et nyt spil (1) eller gå tilbage til hovedmenuen (2).
+        {
+            Console.WriteLine();
+            Console.WriteLine("1: New Battle! -> FOR GLORY!!!");
+            Console.WriteLine("2: Return to main menu");
+
+            string choice = Console.ReadLine(); // Spilleren taster "1" eller "2".
+
+            return choice;
+        }
+        public static void NukeEnemyFleet(char[,] computerBoard) // Her ses "NUKE" funktionen. 
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (computerBoard[i, j] == 'x')
+                    {
+                        computerBoard[i, j] = 'H'; // Markerer alle skibe som ramt/sunket.
+                    }
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("NUKE LAUNCHED! the enemy fleet has been obliterated, Sir!"); // Rød tekst som oplyser spilleren om, at han/hun har vundet spillet med en "NUKE". 
+            Console.ResetColor();
+            Thread.Sleep(3000);
+        }
     }
 }
+
+// Nedenstående viser "fog of war" funktionen, som bare direkte kan erstatte "PrintBoard(computerBoard)".
+// Jeg undlader det i min kode for nu, da jeg hellere vil fremvise computerens evne til at vælge sine skibsplaceringer.
+// Derudover ønsker jeg i vores video let at kunne fremvise et hit, uden at skulle lede efter computerens skibe haha.
+
+// Void PrintFogOfWar(char[,] board)
+// {
+//     for (int i = 0; i < 10; i++)
+//     {   
+//          for (int j = 0; j < 10; j++)
+//          {
+//              if (board[i, j] == 'H')
+//              Console.Write("H ");  // Et skib er ramt og positionen vises nu på brættet som et "H" for "Hit"
+//              else if (board[i, j] == 'M')
+//              Console.Write("M ");  // Intet skib er ramt og positionen vises nu på brættet som et "M" for "Miss"
+//              else
+//              Console.Write("0 "); // Skjuler skibe som tomme felter.
+//          }
+//          Console.WriteLine();
+//      }
+// }
